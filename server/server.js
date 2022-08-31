dd03dfdc8099175362d96b4ff30f73c3d8796041
Jessica Roland <jlroland@user.noreply.github.com>
2022-08-30 19:35:27 -0700

    Set up database in MongoDB Atlas; test connection to server using sample data

--- server/server.js
+++ server/server.js
@@ -2,19 +2,24 @@
 
 const express = require('express');
 const cors = require('cors');
-//const dbo = require('mongodb');
 const app = express();
 app.use(cors());
-// app.use(express.json());
-// app.use(require('./routes/record'));
-// const dbo = require('./db/conn');
+app.use(express.json());
 
-require('dotenv').config();
+require('dotenv').config({path: './config.env'});
 const port = process.env.PORT || 5000;
 
+const { MongoClient } = require('mongodb');
+const dbClient = new MongoClient(process.env.DATABASE_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+// app.use(require('./routes/record'));
+
 app.listen(port, () => {
-//   dbo.connectToServer(function(err) {
-//     if (err) console.error(err);
-//   });
+  dbClient.connect(err => {
+    const collection = dbClient.db('sample_mflix').collection('movies');
+    if (collection) {
+      console.log('Successfully connected to MongoDB');
+    }
+    dbClient.close();
+  });
   console.log(`Server is running on port: ${port}`);
 });
