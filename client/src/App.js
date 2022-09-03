3fc7fbf15dc4531fcce530fbc56765061394b1cb
Jessica Roland <jlroland@user.noreply.github.com>
2022-09-03 11:12:08 -0700

    Build up basic form and event handling for login and calculator

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
 import './css/App.css';
-import Queries from './components/savedQueries';
+//import Queries from './components/savedQueries';
 //import Login from './components/login';
-//import Calculator from './components/calculator';
+import Calculator from './components/calculator';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Queries />
+      <Calculator />
       <Footer />
     </div>
   );
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,18 +3,31 @@ import React from 'react';
 class Login extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {};
+      this.state = {
+        username: '',
+        password: ''
+      };
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
+
+    handleChange(event) {
+      this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+      event.preventDefault()
+    }
+   
     render() {
       return (
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <p>Enter username and password</p>
           <label>Username:
-            <input type='text'/>
+            <input name='username' type='text' value={this.state.username} onChange={this.handleChange}/>
           </label>
           <label>Password:
-            <input type='text'/>
+          <input name='password' type='text' value={this.state.password} onChange={this.handleChange}/>
           </label>
           <input type='submit' value='Sign In'/>
           <input type='submit' value='Create Account'/>
